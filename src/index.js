e5cae131f08951f2bc48448d7a972b3d40508a93
github user name <tomo.zu_shima@icloud.com>
2022-09-20 17:26:44 +0900

    component_square

--- src/index.js
+++ src/index.js
@@ -2,26 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
+// class Square extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             value: null,
+//         };
+//     }
+//     render() {
+//       return (
+//         <button
+//         className="square"
+//         onClick={() => this.setState({value: 'X'})}>
+//           {this.state.value}
+//         </button>
+//       );
+//     }
+//   }
+  class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            value: null,
+            squares: Array(9).fill(null),
         };
     }
-    render() {
-      return (
-        <button
-        className="square"
-        onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-      );
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
-  }
-  class Board extends React.Component {
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (<Square value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+      );
     }
     render() {
       const status = 'Next player: X';
@@ -63,7 +79,6 @@ class Square extends React.Component {
       );
     }
   }
-
   // ========================================
 
   const root = ReactDOM.createRoot(document.getElementById("root"));
